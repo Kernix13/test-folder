86153491def96c761e1402105440e4018b54b1cd
“Jim” <jimkernicky@gmail.com>
2022-06-16 19:01:07 -0400

    Deleted script3.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// I need to import these somehow, they can't be in each config component
+// I need to import these, they can't be in each config component
 const square = [
   [5, 5],
   [6, 6],
@@ -54,9 +54,12 @@ const rectangle = [
     ]
   }
 ];
+
 const allTarps = [
   {
-    "Rectangle 1:2": [
+    tarpCategory: "Rectangle",
+    tarpRatio: "1:2",
+    tarpSizes: [
       [6, 12],
       [7, 14],
       [8, 16],
@@ -64,7 +67,18 @@ const allTarps = [
     ]
   },
   {
-    "Rectangle 2:3": [
+    tarpCategory: "Rectangle",
+    tarpRatio: "3:5",
+    tarpSizes: [
+      [6, 10],
+      [9, 15],
+      [12, 20]
+    ]
+  },
+  {
+    tarpCategory: "Rectangle",
+    tarpRatio: "2:3",
+    tarpSizes: [
       [6, 9],
       [8, 12],
       [10, 15],
@@ -72,21 +86,18 @@ const allTarps = [
     ]
   },
   {
-    "Rectangle 3:4": [
+    tarpCategory: "Rectangle",
+    tarpRatio: "3:4",
+    tarpSizes: [
       [6, 8],
       [9, 12],
       [12, 16]
     ]
   },
   {
-    "Rectangle 3:5": [
-      [6, 10],
-      [9, 15],
-      [12, 20]
-    ]
-  },
-  {
-    "Rectangle 4:5": [
+    tarpCategory: "Rectangle",
+    tarpRatio: "4:5",
+    tarpSizes: [
       [4, 5],
       [8, 10],
       [12, 15],
@@ -94,23 +105,79 @@ const allTarps = [
     ]
   },
   {
-    "Other rectangle": [
+    tarpCategory: "Rectangle",
+    tarpRatio: "Odd",
+    tarpSizes: [
       [5, 7],
       [7, 9]
     ]
   },
   {
-    Square: [
+    tarpCategory: "Square",
+    tarpRatio: "1:1",
+    tarpSizes: [
       [5, 5],
       [6, 6],
       [7, 7],
       [8, 8],
       [9, 9],
-      [10, 10]
+      [10, 10],
+      [12, 12],
+      [15, 15],
+      [20, 20]
     ]
   }
 ];
-console.log(rectangle.rect4to5);
+
+console.log(allTarps[0]);
+
+const tarps = [
+  [
+    [6, 12],
+    [7, 14],
+    [8, 16],
+    [9, 18],
+    [10, 20]
+  ],
+  [
+    [6, 10],
+    [9, 15],
+    [12, 20]
+  ],
+  [
+    [6, 9],
+    [8, 12],
+    [10, 15],
+    [12, 18]
+  ],
+  [
+    [6, 8],
+    [9, 12],
+    [12, 16]
+  ],
+  [
+    [4, 5],
+    [8, 10],
+    [12, 15],
+    [16, 20]
+  ],
+  [
+    [5, 7],
+    [7, 9]
+  ],
+  [
+    [5, 5],
+    [6, 6],
+    [7, 7],
+    [8, 8],
+    [9, 9],
+    [10, 10],
+    [12, 12],
+    [15, 15],
+    [20, 20]
+  ]
+];
+// console.log(tarps[1]);
 
 // Global state and variables
 const height = 74;
@@ -122,75 +189,90 @@ const sitHeight = height / 2;
 const sitDepth = (height * 7) / 32;
 const chairDepth = (height * 13) / 32;
 
-// These need to be dynamic
-const tarpSize = [96, 120];
-const angle = 30;
-const len = tarpSize[0];
-const wid = tarpSize[1];
-
-const sleepClear = len - height; // must be > 0
-
-// I need a component for EACH configuration, then I need to use a for loop for first square tarps, then rectangle tarps, but also a nested for loop to test incrementally the min angle to the max angle
-function mainCalc() {}
-const lean_to = {
-  configName: "Lean-To",
-  type: "Lean-To",
-  // for loop
-  angles: [30, 60],
-  tarpTypes: [square, rectangle],
-  sleepClear: len - height,
-  cover(angle) {
-    return Math.round(Math.cos(angle * deg2Rad) * wid);
-  },
-  ridgeHeight(angle) {
-    return Math.round(Math.sin(angle * deg2Rad) * wid);
-  },
-  sitTarpHt(angle) {
-    return Math.round(Math.tan(angle * deg2Rad) * sitCover);
-  },
-  chairTarpHt(angle) {
-    return Math.round(Math.tan(angle * deg2Rad) * chairCover);
-  }
-};
-
-const cover = lean_to.cover(lean_to.angles[0]);
-const ridgeHt = lean_to.ridgeHeight(lean_to.angles[0]);
-const ridgeHtClear = ridgeHt - sitHeight; // must be > 0
-
-// sitCover is to determine the height of the tarp at the user's sitHeight
-const sitCover = Math.round(cover - sitDepth);
-const sitTarpHt = lean_to.sitTarpHt(lean_to.angles[0]);
-const sitTarpHtClear = sitTarpHt - sitHeight; // must be > 0
-
-// chairCover is to determine the height of the tarp at the user's chairHeight
-const chairCover = cover - chairDepth;
-const chairTarpHt = lean_to.chairTarpHt(lean_to.angles[0]);
-const chairTarpHtClear = chairTarpHt - chairHeight; // must be > 0
-
-console.log(lean_to.angles);
-console.log(square[1][0]);
+// Configuration constants
+const configName = "Lean-To";
+const configType = "Lean-To";
+const configAngles = [30, 45];
+const configTarps = [square, rectangle];
 
 let matches = [];
-let match = [];
-function something() {
-  for (let i = lean_to.angles[0]; i <= lean_to.angles[1]; i++) {
-    square.forEach((item, index) => {
-      let len = square[index][0];
-      let wid = square[index][1];
-      const sleepClear = len * 12 - height; // must be > 0
+let subset = [];
+let outputObj = [];
+let finalObj = [];
+let sleepClr = 0;
+for (let i = 0; i < tarps.length; i++) {
+  for (let j = 0; j < tarps[i].length; j++) {
+    let len = tarps[i][j][0];
+    sleepClr = len * 12 - height; // must be > 0
+    if (sleepClr > 4) {
+      // REMOVE MATCHES?
+      matches.push(tarps[i][j]);
+      if (tarps[i].indexOf(tarps[i][j]) && !subset.includes(tarps[i][j])) {
+        subset.push(tarps[i][j]);
 
-      if (i === 45 && sleepClear > 0) {
-        matches.push(square.indexOf(item));
-        match = square[Math.min(matches)];
-        console.log("match 1: " + match);
-        return match;
+        /* I commented the break out because I want "all" the tarps */
+        // if (tarps[i].includes(subset[i])) {
+        //   break;
+        // }
       }
-      console.log("match 2: " + match);
-    });
-    console.log("match 3: " + match);
+    }
   }
-  console.log("match 4: " + match);
-  return match;
 }
-// Why can't I get it?
-console.log(something());
+
+let tarpMatches = [];
+// let angles = [];
+let first = [];
+let ridgeLines = [];
+let tarpAngles = [];
+let importantThings = {};
+
+let cover = 0;
+let ridgeHeight = 0;
+let sitCover = 0;
+let sitTarpHt = 0;
+let sitTarpHtClear = 0;
+let chairCover = 0;
+let chairTarpHt = 0;
+let chairTarpHtClear = 0;
+
+subset.forEach(item => {
+  let wid = item[1] * 12;
+  let len = item[0] * 12;
+
+  for (let angle = configAngles[1]; angle >= configAngles[0]; angle--) {
+    let sleepClear = len - height;
+    cover = Math.round(Math.cos(angle * deg2Rad) * wid);
+    ridgeHt = Math.round(Math.sin(angle * deg2Rad) * wid);
+    // ridgeHeight = Math.round(Math.sin(angle * deg2Rad) * wid);
+    ridgeHeight = Math.min(ridgeHt, height + 6);
+
+    // **** NEED TO RECALCULATE THESE 3 IF RIDGEHEIGHT = HEIGHT + 6 WHICH MEANS COVER HAS TO BE RECALULATED AS WELL
+    sitCover = Math.round(cover - (sitDepth + 3));
+    sitTarpHt = Math.round(Math.tan(angle * deg2Rad) * sitCover);
+    sitTarpHtClear = sitTarpHt - sitHeight; // must be > 0
+
+    chairCover = cover - chairDepth;
+    chairTarpHt = Math.round(Math.tan(angle * deg2Rad) * chairCover);
+    chairTarpHtClear = chairTarpHt - chairHeight; // ideally > 0
+
+    // Changing the # gives a different angle
+    if (sitTarpHtClear > 3) {
+      outputObj = [item].concat({ sleepClear, cover, ridgeHeight, sitTarpHtClear, chairTarpHtClear, angle });
+      // How do I break when first angle matches requirements?
+      // No I want the smallest angle possible or else the ridgeline will be too high -
+      // if (ridgeHeight > height + 3) {
+
+      // }
+    }
+  }
+  finalObj.push(outputObj);
+});
+// GOT IT!
+console.log(finalObj);
+
+finalObj.forEach(item => {
+  if (item.length !== 0) {
+    console.log(item);
+  }
+  // new cover = need new angle as opposite (ridgeheight) / hyp, then use that angle to find cover, then calcualte sitTarpHtClear
+});
--- script2.js
+++ script2.js
@@ -1,63 +1,10 @@
-// I need to import these somehow, they can't be in each config component
-const square = [
-  [5, 5],
-  [6, 6],
-  [7, 7],
-  [8, 8],
-  [9, 9],
-  [10, 10]
-];
-
-const rectangle = [
-  {
-    "Rectangle 1:2": [
-      [6, 12],
-      [7, 14],
-      [8, 16],
-      [10, 20]
-    ]
-  },
-  {
-    rect2to3: [
-      [6, 9],
-      [8, 12],
-      [10, 15],
-      [12, 18]
-    ]
-  },
-  {
-    rect3to4: [
-      [6, 8],
-      [9, 12],
-      [12, 16]
-    ]
-  },
-  {
-    rect3to5: [
-      [6, 10],
-      [9, 15],
-      [12, 20]
-    ]
-  },
-  {
-    rect4to5: [
-      [4, 5],
-      [8, 10],
-      [12, 15],
-      [16, 20]
-    ]
-  },
-  {
-    rectOdd: [
-      [5, 7],
-      [7, 9]
-    ]
-  }
-];
+// I need to import these, they can't be in each config component
 
 const allTarps = [
   {
-    "Rectangle 1:2": [
+    tarpCategory: "Rectangle",
+    tarpRatio: "1:2",
+    tarpSizes: [
       [6, 12],
       [7, 14],
       [8, 16],
@@ -65,7 +12,18 @@ const allTarps = [
     ]
   },
   {
-    "Rectangle 2:3": [
+    tarpCategory: "Rectangle",
+    tarpRatio: "3:5",
+    tarpSizes: [
+      [6, 10],
+      [9, 15],
+      [12, 20]
+    ]
+  },
+  {
+    tarpCategory: "Rectangle",
+    tarpRatio: "2:3",
+    tarpSizes: [
       [6, 9],
       [8, 12],
       [10, 15],
@@ -73,21 +31,18 @@ const allTarps = [
     ]
   },
   {
-    "Rectangle 3:4": [
+    tarpCategory: "Rectangle",
+    tarpRatio: "3:4",
+    tarpSizes: [
       [6, 8],
       [9, 12],
       [12, 16]
     ]
   },
   {
-    "Rectangle 3:5": [
-      [6, 10],
-      [9, 15],
-      [12, 20]
-    ]
-  },
-  {
-    "Rectangle 4:5": [
+    tarpCategory: "Rectangle",
+    tarpRatio: "4:5",
+    tarpSizes: [
       [4, 5],
       [8, 10],
       [12, 15],
@@ -95,67 +50,31 @@ const allTarps = [
     ]
   },
   {
-    "Other rectangle": [
+    tarpCategory: "Rectangle",
+    tarpRatio: "Odd",
+    tarpSizes: [
       [5, 7],
       [7, 9]
     ]
   },
   {
-    Square: [
+    tarpCategory: "Square",
+    tarpRatio: "1:1",
+    tarpSizes: [
       [5, 5],
       [6, 6],
       [7, 7],
       [8, 8],
       [9, 9],
-      [10, 10]
+      [10, 10],
+      [12, 12],
+      [15, 15],
+      [20, 20]
     ]
   }
 ];
 
-const tarps = [
-  [
-    [5, 5],
-    [6, 6],
-    [7, 7],
-    [8, 8],
-    [9, 9],
-    [10, 10]
-  ],
-  [
-    [6, 12],
-    [7, 14],
-    [8, 16],
-    [9, 18],
-    [10, 20]
-  ],
-  [
-    [6, 9],
-    [8, 12],
-    [10, 15],
-    [12, 18]
-  ],
-  [
-    [6, 8],
-    [9, 12],
-    [12, 16]
-  ],
-  [
-    [6, 10],
-    [9, 15],
-    [12, 20]
-  ],
-  [
-    [4, 5],
-    [8, 10],
-    [12, 15],
-    [16, 20]
-  ],
-  [
-    [5, 7],
-    [7, 9]
-  ]
-];
-// console.log(tarps[1]);
+// console.log(allTarps[0].tarpSizes);
 
 // Global state and variables
 const height = 74;
@@ -170,25 +89,33 @@ const chairDepth = (height * 13) / 32;
 // Configuration constants
 const configName = "Lean-To";
 const configType = "Lean-To";
-const configAngles = [30, 60];
-const configTarps = [square, rectangle];
+const configAngles = [30, 45];
+// const configTarps = [square, rectangle];
 
 let matches = [];
-let sleepClear = 0;
-for (let i = 0; i < tarps.length; i++) {
-  for (let j = 0; j < tarps[i].length; j++) {
-    let len = tarps[i][j][0];
-    sleepClear = len * 12 - height; // must be > 0
-    if (sleepClear > 4) {
-      let index = tarps[i].indexOf(tarps[i][j]);
-      // need arr.find() I think
-      matches.push(tarps[i][j]);
-      // console.log(tarps[i][j][0] * 12, tarps[i][j], index);
+let subset = [];
+let outputObj = [];
+let finalObj = [];
+let sleepClr = 0;
+for (let i = 0; i < allTarps.length; i++) {
+  for (let j = 0; j < allTarps[i].tarpSizes.length; j++) {
+    let len = allTarps[i]["tarpSizes"][j][0];
+    sleepClr = len * 12 - height; // must be > 0
+    if (sleepClr > 4) {
+      // REMOVE MATCHES?
+      matches.push(allTarps[i]["tarpSizes"][j]);
+      if (allTarps[i]["tarpSizes"].indexOf(allTarps[i]["tarpSizes"][j]) && !subset.includes(allTarps[i]["tarpSizes"][j])) {
+        subset.push(allTarps[i]["tarpSizes"][j]);
+
+        /* I commented the break out because I want "all" the allTarps */
+        // if (allTarps[i].includes(subset[i])) {
+        //   break;
+        // }
+      }
     }
   }
 }
-console.log(matches);
-
+console.log("subset: ", subset);
 let tarpMatches = [];
 // let angles = [];
 let first = [];
@@ -198,62 +125,51 @@ let importantThings = {};
 
 let cover = 0;
 let ridgeHeight = 0;
-let ridgeHtClear = 0;
 let sitCover = 0;
 let sitTarpHt = 0;
 let sitTarpHtClear = 0;
 let chairCover = 0;
 let chairTarpHt = 0;
 let chairTarpHtClear = 0;
-// let outputObj = {}
 
-matches.forEach(item => {
+subset.forEach(item => {
   let wid = item[1] * 12;
-  const angles = [30, 35, 40, 45, 50, 55, 60];
-  const angle = 45;
-  cover = Math.round(Math.cos(angle * deg2Rad) * wid);
-  ridgeHeight = Math.round(Math.sin(angle * deg2Rad) * wid);
-  ridgeHtClear = ridgeHeight - sitHeight; // must be > 0
-  sitCover = Math.round(cover - sitDepth);
-  sitTarpHt = Math.round(Math.tan(angle * deg2Rad) * sitCover);
-  sitTarpHtClear = sitTarpHt - sitHeight; // must be > 0
-  chairCover = cover - chairDepth;
-  chairTarpHt = Math.round(Math.tan(angle * deg2Rad) * chairCover);
-  chairTarpHtClear = chairTarpHt - chairHeight; // must be > 0
+  let len = item[0] * 12;
 
-  let outputObj = item.concat({ wid, sleepClear, ridgeHeight, ridgeHtClear, sitTarpHtClear, chairTarpHtClear });
+  for (let angle = configAngles[1]; angle >= configAngles[0]; angle--) {
+    let sleepClear = len - height;
+    cover = Math.round(Math.cos(angle * deg2Rad) * wid);
+    ridgeHt = Math.round(Math.sin(angle * deg2Rad) * wid);
+    // ridgeHeight = Math.round(Math.sin(angle * deg2Rad) * wid);
+    ridgeHeight = Math.min(ridgeHt, height + 6);
 
-  if (sitTarpHtClear > 0) {
-    // console.log(outputObj);
-    // console.log(chairTarpHtClear);
-    // return outputObj;
-    let outputObj = item.concat({ wid, sleepClear, ridgeHeight, ridgeHtClear, sitTarpHtClear, chairTarpHtClear });
+    // **** NEED TO RECALCULATE THESE 3 IF RIDGEHEIGHT = HEIGHT + 6 WHICH MEANS COVER HAS TO BE RECALULATED AS WELL
+    sitCover = Math.round(cover - (sitDepth + 3));
+    sitTarpHt = Math.round(Math.tan(angle * deg2Rad) * sitCover);
+    sitTarpHtClear = sitTarpHt - sitHeight; // must be > 0
+
+    chairCover = cover - chairDepth;
+    chairTarpHt = Math.round(Math.tan(angle * deg2Rad) * chairCover);
+    chairTarpHtClear = chairTarpHt - chairHeight; // ideally > 0
+
+    // Changing the # gives a different angle
+    if (sitTarpHtClear > 3) {
+      outputObj = [item].concat({ sleepClear, cover, ridgeHeight, sitTarpHtClear, chairTarpHtClear, angle });
+      // How do I break when first angle matches requirements?
+      // No I want the smallest angle possible or else the ridgeline will be too high -
+      // if (ridgeHeight > height + 3) {
+
+      // }
+    }
   }
-  console.log(outputObj);
-  // for (let i = configAngles[0]; i <= configAngles[1]; i++) {
-  //   cover = Math.round(Math.cos(i * deg2Rad) * wid);
-  //   ridgeHeight = Math.round(Math.sin(i * deg2Rad) * wid);
-  //   ridgeHtClear = ridgeHeight - sitHeight; // must be > 0
-  //   sitCover = Math.round(cover - sitDepth);
-  //   sitTarpHt = Math.round(Math.tan(i * deg2Rad) * sitCover);
-  //   sitTarpHtClear = sitTarpHt - sitHeight; // must be > 0
-  //   chairCover = cover - chairDepth;
-  //   chairTarpHt = Math.round(Math.tan(i * deg2Rad) * chairCover);
-  //   chairTarpHtClear = chairTarpHt - chairHeight; // must be > 0
-
-  //   if (sitTarpHtClear > 1) {
-  //     ridgeLines.push(ridgeHeight);
-  //     tarpMatches.push(matches[item]);
-  //     // importantThings = { lineHeight: ridgeLines[j], tarpSizes: tarpMatches[j] };
-  //     first.push(tarpMatches.indexOf(matches[item]));
-  //   }
-  // }
-  // console.log(outputObj);
+  finalObj.push(outputObj);
 });
-// console.log(outputObj);
+// GOT IT!
+// console.log(finalObj);
 
-// How to turn 2 arrays into an object: 1. https://bobbyhadz.com/blog/javascript-create-object-from-two-arrays. 2. https://stackoverflow.com/questions/39127989/create-an-object-from-an-array-of-keys-and-an-array-of-values, 3. https://www.geeksforgeeks.org/how-to-create-an-object-from-two-arrays-in-javascript/, 4. https://www.tutorialspoint.com/how-to-combine-two-arrays-into-an-array-of-objects-in-javascript, 5.
-// console.log(importantThings);
-// console.log(tarpAngles);
-
-// Variables dependent on for loop or forEach
+finalObj.forEach(item => {
+  if (item.length !== 0) {
+    console.log(item);
+  }
+  // new cover = need new angle as opposite (ridgeheight) / hyp, then use that angle to find cover, then calcualte sitTarpHtClear
+});
